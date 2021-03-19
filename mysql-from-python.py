11316cc015c7741b3e2a24e74228b3ccd2034a44
Brianconn71 <Brian.connolly71@gmail.com>
2021-03-19 09:26:52 +0000

    finished mysql section

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,10 +14,10 @@ connection = pymysql.connect(host='localhost',
 try:
     #run a query
     with connection.cursor() as cursor:
-        rows = [("Bob", 21, "1994-02-06 23:04:56"),
-                ("Jim", 45, "1974-02-06 20:04:56"),
-                ("Paul", 50, "1974-02-06 20:04:56")]
-        cursor.executemany("Insert into Friends Values(%s, %s, %s);", rows)
+        list_of_names = ['Paul', 'frank']
+        #prepare a string with the same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute("delete from Friends where name in ({});".format(format_strings), list_of_names) #string interpolation %s
         connection.commit()
         
 finally:
